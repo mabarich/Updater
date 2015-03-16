8fa491f7683494fc87281777b7d5fe29bb529efb
mabarich <marco.barichello.2@live.com>
2015-03-16 18:09:49 +0100

    Updater

---
+++ Updater/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aggiornamento.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 32);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Updater/Form1.cs
@@ -0,0 +1,122 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            updt();
+        }
+
+        public void updt()
+        {
+            string ultimaModificaS = "";
+            label1.Text = "Download in corso.";
+            using (WebClient Client = new WebClient())
+            {
+                Client.DownloadFile("http://maktheeater.altervista.org/Uscite_Manga.exe", "Uscite Manga.exe");
+            }
+            label1.Text = "Download completato.";
+
+            string url1 = "http://maktheeater.altervista.org/UltimoAggiornamento.html";
+            var webGet1 = new HtmlWeb();
+            var document1 = webGet1.Load(url1);
+            HtmlNodeCollection tl1 = document1.DocumentNode.SelectNodes("//p");
+            if (tl1 != null)
+            {
+                foreach (HtmlAgilityPack.HtmlNode node in tl1)
+                {
+                    ultimaModificaS = node.InnerText;
+                }
+            }
+            try
+            {
+                StreamWriter sw = new StreamWriter("UltimaModifica.txt", false, Encoding.GetEncoding(1252));
+                sw.Write(ultimaModificaS);
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+            var process = new Process();
+            process.StartInfo.FileName = "Uscite Manga.exe";
+            process.Start();
+            Environment.Exit(0);
+        }
+
+        /*
+        private void btn_Replace_Click(object sender, EventArgs e)
+
+        {
+
+            str_Source = txtSource.Text;
+
+            str_Destination = txtDestination.Text;
+
+            str_backup = txtBackup.Text;
+
+            if (str_Source == null || str_Destination == null)
+
+            {
+
+                MessageBox.Show("SOUC OR DETNATION FILE PATH MISSING");
+
+            }
+
+            else
+
+            {
+
+                if (File.Exists(str_Source) == true)
+
+                {
+
+                    if (File.Exists(str_Destination) == true)
+
+                    {
+
+                        File.Replace(str_Source, str_Destination, str_backup);
+
+                        MessageBox.Show("Data REPLACED");
+
+                    }
+
+                    else
+
+                    {
+
+                         MessageBox.Show("FILE NOT EXISTED");
+
+                    }
+
+               }
+
+                else
+
+                    MessageBox.Show("SOURCE FILE DOES NOT EXIST");
+
+            }
+
+        }  
+        */
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
